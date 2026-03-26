8bf4054d5893b0008eea6a3d39fd22d3dca0b6b7
dambar08 <dambar1111@gmail.com>
2026-03-26 10:31:05 +0800

    add: log application detail during startup

--- src/main/java/com/open/management/ManagementApplication.java
+++ src/main/java/com/open/management/ManagementApplication.java
@@ -1,10 +1,18 @@
 package com.open.management;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ })
@@ -12,8 +20,54 @@ public class ManagementApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ManagementApplication.class);
 
+	private final Environment env;
+	public static final Marker CRLF_SAFE_MARKER = MarkerFactory.getMarker("CRLF_SAFE");
+
+	public ManagementApplication(final Environment env) {
+		this.env = env;
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(ManagementApplication.class, args);
+		var app = new SpringApplication(ManagementApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logApplicationStartup(env);
+	}
+
+	private static void logApplicationStartup(Environment env) {
+		String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store"))
+		.map(key -> "https")
+		.orElse("http");
+		String applicationName = env.getProperty("spring.application.name");
+		String serverPort = env.getProperty("server.port");
+		String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path"))
+            .filter(StringUtils::isNotBlank)
+            .orElse("/");
+		var hostAddress = "localhost";
+		try {
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		}catch(UnknownHostException e) {
+			LOG.warn("The host could not be determined, using `localhost` as fallback");
+		}
+
+		LOG.info(
+            CRLF_SAFE_MARKER,
+            """
+            ----------------------------------------------------------
+            \tApplication '{}' is running! Access URLs:
+            \tLocal: \t\t{}://localhost:{}{}
+            \tExternal: \t{}://{}:{}{}
+            \tProfile(s): \t{}
+            ----------------------------------------------------------""",
+            applicationName,
+            protocol,
+            serverPort,
+            contextPath,
+            protocol,
+            hostAddress,
+            serverPort,
+            contextPath,
+            env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
+        );
 	}
 
 }
